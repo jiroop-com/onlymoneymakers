402ec0f38e6d1c225b43e44120012800c7044fb5
Jonas-rbg <jonashaavisto@gmail.com>
2024-06-23 19:53:02 +0300

    updatte

--- blog.config.js
+++ blog.config.js
@@ -20,13 +20,13 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Latest Community Updates, Latest News Posts, Welcome to Mindset Mastery',
+    'Latest Community Updates, Latest News Posts, Welcome',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Mindset', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jiroop', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Stay elevated!', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://mindset.onthewifi.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://jiroop.com', // 网站地址
   KEYWORDS:
     process.env.NEXT_PUBLIC_KEYWORD ||
     'mindset, news, blog, trading, e-commerce, health, fitness, business', // 网站关键词 英文逗号隔开
@@ -445,7 +445,7 @@ const BLOG = {
   ANALYTICS_BAIDU_ID: process.env.NEXT_PUBLIC_ANALYTICS_BAIDU_ID || '', // e.g 只需要填写百度统计的id，[baidu_id] -> https://hm.baidu.com/hm.js?[baidu_id]
   ANALYTICS_CNZZ_ID: process.env.NEXT_PUBLIC_ANALYTICS_CNZZ_ID || '', // 只需要填写站长统计的id, [cnzz_id] -> https://s9.cnzz.com/z_stat.php?id=[cnzz_id]&web_id=[cnzz_id]
   ANALYTICS_GOOGLE_ID:
-    process.env.NEXT_PUBLIC_ANALYTICS_GOOGLE_ID || 'G-09RMV566WX', // 谷歌Analytics的id e.g: G-XXXXXXXXXX
+    process.env.NEXT_PUBLIC_ANALYTICS_GOOGLE_ID || 'G-KG2ZCDQRDJ', // 谷歌Analytics的id e.g: G-XXXXXXXXXX
 
   // 51la 站点统计 https://www.51.la/
   ANALYTICS_51LA_ID: process.env.NEXT_PUBLIC_ANALYTICS_51LA_ID || '', // id，在51la后台获取 参阅 https://docs.tangly1024.com/article/notion-next-51-la
