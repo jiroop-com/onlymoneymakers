8ebcb7d308991b356130b036d80862b37cc8d4ca
Jonas-rbg <jonashaavisto@gmail.com>
2024-06-25 19:09:39 +0300

    minor changes to data

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@
 import BLOG from '@/blog.config'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -16,19 +15,18 @@ class MyDocument extends Document {
           async
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8437419081527400'
           crossorigin='anonymous'></script>
-        <meta name='yandex-verification' content='622aa0408012f881' />
+        <meta name='yandex-verification' content='f9bc4558c3b54f76' />
         <script src='https://yandex.ru/ads/system/context.js' async></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
             window.yaContextCb = window.yaContextCb || [];
-            window.lemonSqueezyAffiliateConfig = { store: "mindset-store" };
+            window.lemonSqueezyAffiliateConfig = { store: "jiroop-store" };
           `
           }}
         />
         <script src='https://lmsqueezy.com/affiliate.js' defer></script>
         <script src='https://gumroad.com/js/gumroad.js'></script>
-        <script src='https://cdn.jsdelivr.net/gh/Mindset-Community/live2d-widget@latest/autoload.js'></script>
 
         <Head>
           {/* 预加载字体 */}
