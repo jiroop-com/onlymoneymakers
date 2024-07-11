b0df102f82f033ca4d5ca2a29615c7561f3f1f3d
Jonas-rbg <jonashaavisto@gmail.com>
2024-07-11 23:07:38 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,33 @@ class MyDocument extends Document {
         />
         <script src='https://lmsqueezy.com/affiliate.js' defer></script>
         <script src='https://gumroad.com/js/gumroad.js'></script>
-        <script src='https://cdn.jsdelivr.net/gh/Mindset-Community/live2d-widget@latest/autoload.js'></script>
+        <script
+          async
+          src='https://fundingchoicesmessages.google.com/i/pub-8437419081527400?ers=1'
+          nonce='N5LV6_wS_cDM1awWc4pNhA'></script>
+        <script
+          nonce='N5LV6_wS_cDM1awWc4pNhA'
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function() {
+              function signalGooglefcPresent() {
+                if (!window.frames['googlefcPresent']) {
+                  if (document.body) {
+                    const iframe = document.createElement('iframe');
+                    iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                    iframe.style.display = 'none';
+                    iframe.name = 'googlefcPresent';
+                    document.body.appendChild(iframe);
+                  } else {
+                    setTimeout(signalGooglefcPresent, 0);
+                  }
+                }
+              }
+              signalGooglefcPresent();
+            })();
+          `
+          }}
+        />
 
         <Head>
           {/* 预加载字体 */}
